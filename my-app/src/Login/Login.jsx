c544a967aecdc5f14acbd519bb1523ea906bd74e
neriyaMalachi <neriyamalachi@gmail.com>
2022-08-22 12:34:22 +0300

    id the login

--- my-app/src/Login/Login.jsx
+++ my-app/src/Login/Login.jsx
@@ -1,20 +1,53 @@
 import React from "react";
 import { Input, Button } from "@chakra-ui/react";
 import "./Login.css";
-
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 function Login() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  useEffect(() => {
+    if (localStorage.getItem("user-info")) {
+      history.push("/login");
+    }
+  }, []);
+function login(){
+
+  console.log(name,password);
+  let item={name,password}
+  let resolt =await fetch("/*insert API*/",{
+    method:'POST',
+    headers:{
+      
+    },
+    body:JSON.stringify(item)
+  });
+  resolt=await resolt.json();
+  localStorage.setItem("",JSON.stringify(resolt))
+history.push("/login")
+}
+
   return (
     <div className="body">
       <div className="divForInput">
-        
-        <Input className="input" placeholder="name" />
-        <Input className="input" placeholder="pasword" />
+        <Input
+          type="text"
+          className="input"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          type="password"
+          className="input"
+          placeholder="pasword"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <div className="butoon">
-        <Button colorScheme='gray'>Log in</Button>
+          <Button onClick={login} colorScheme="gray">Log in</Button>
         </div>
-        
       </div>
-     
     </div>
   );
 }
