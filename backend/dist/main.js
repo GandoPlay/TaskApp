0dde31289db40dbe544a67153acf81f6f7478447
Gal-hub <galcohen27@gmail.com>
2022-08-22 16:15:17 +0300

    connection with react

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -4,6 +4,19 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: [
+            'http://localhost:3000',
+            'http://example.com',
+            'http://www.example.com',
+            'http://app.example.com',
+            'https://example.com',
+            'https://www.example.com',
+            'https://app.example.com',
+        ],
+        methods: ["GET", "POST"],
+        credentials: true,
+    });
     await app.listen(3001);
 }
 bootstrap();
