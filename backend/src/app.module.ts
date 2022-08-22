e4f6763fc9deaed76ae3f58a8e16265ddd00c522
Gal-hub <galcohen27@gmail.com>
2022-08-22 12:09:31 +0300

    Ido gay

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,6 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule, MongooseModule.forRoot(
-    'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.4')],
-
+    'mongodb://localhost/appdb')],
 })
 export class AppModule {}
