fb117545c0543dc69ed1b6cda9685ede74ce03f9
GandoPlay <edo22061@gmail.com>
2022-08-23 09:45:30 +0300

    changed intefaces

--- backend/src/auth/auth.model.ts
+++ backend/src/auth/auth.model.ts
@@ -1,39 +1,39 @@
 import * as mongoose from 'mongoose';
-enum typeTask {
-    NIGHT,
-    CLEAN,
-    AVTASH
-}
-export const UserSchema = new mongoose.Schema({
+enum Role {
+  AVTASH = 'AVTASH',
+  CLEAN = 'CLEAN',
+  NIGHT = 'NIGHT'
+} 
+
+export const authSchema = new mongoose.Schema({
   username: { type: String, required: true },
   hash: { type: String, required: true },
-  password: { type: String, required: false },
-  rating: { type: Number, required: false },
 
 });
 
 
-// export const TaskSchema = new mongoose.Schema({
-//     rating: { type: Number, required: true },
-//     date: { type: Number, required: true },
-//     type: { type: typeTask, required: true },
+export const TaskSchema = new mongoose.Schema({
+    rating: { type: Number, required: true },
+    date: { type: Number, required: true },
+    type: { type: String, enum : Role, required: true },
 
-//   });
+  });
 
 
 
 export interface User extends mongoose.Document{
   id: string;
   username: string;
-  password: string;
   hash: string;
   rating: number;
   tasks: Task[]
 }
+
+
 export interface Task extends mongoose.Document{
     id: string;
     rating: number;
     date: number;
-    type: typeTask
+    type: Role
 
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,12 +2,12 @@ import { Module} from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
-import { UserSchema } from './auth.model';
+import { authSchema } from './auth.model';
 import { AuthService } from './auth.service';
 
 @Module({
     // חיבור לאטלס לפי סיסמא זהות וip
-  imports: [JwtModule.register({}), MongooseModule.forFeature([{name: 'User', schema: UserSchema}])],
+  imports: [JwtModule.register({}), MongooseModule.forFeature([{name: 'User', schema: authSchema}])],
   providers: [AuthService],
   controllers: [AuthController]
 })
