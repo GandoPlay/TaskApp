d711d1534234ea5dd1ec57937dded7e0f50d40f5
neriyaMalachi <neriyamalachi@gmail.com>
2022-09-12 16:48:09 +0300

    finish modal end role map end menu

---
+++ frontend-side/src/Constant.js
@@ -0,0 +1,6 @@
+export const Role = {
+	AVTASH: 'אבטש',
+	CLEAN: 'ניקיון',
+	NIGHT: 'לילה',
+	HANFZA: 'הנפצה'
+}
--- frontend-side/src/DateTable/DateTable.jsx
+++ frontend-side/src/DateTable/DateTable.jsx
@@ -4,14 +4,37 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import { Input, Button, Select, Box, Flex, border } from "@chakra-ui/react";
-
+import {
+  Input,
+  Button,
+  Select,
+  Box,
+  Flex,
+  border,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  MenuItem,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Center,
+  Grid,
+  MenuList,
+  Menu,
+  MenuButton,
+} from "@chakra-ui/react";
 import { DateRange } from "react-date-range";
 import { DayPicker } from "react-day-picker";
 import { format } from "date-fns";
 // import { addDays } from "date-fns";
 // import "react-date-range/dist/styles.css";
 // import "react-date-range/dist/theme/default.css";
+
+const Role = ["אבטש", "ניקיון", "לילה", "הנפצה", "מחסן", "שמירה", "אחר"];
+
 const localizer = momentLocalizer(moment);
 
 const events = [
@@ -37,33 +60,14 @@ function DateTable() {
   const [allEvent, setAllEvent] = useState(events);
   // const [calender, setCalender] = useState("");
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [range, setRange] = useState();
 
-  //   const [open, setOpen] = useState(false);
-  //   const refOne = useRef(null);
-  //   useEffect(() => {
-  //     document.addEventListener("keydown", hideOnEscape, true);
-  //     document.addEventListener("click", hideOnClickOutside, true);
-  //   }, []);
+  const click = () => {};
   const AddEvent = () => {
     // setAllEvent([...allEvent, newEvent]);
   };
-
-  //   const hideOnEscape = (e) => {
-  //     console.log(e.key);
-  //     if (e.key === "Escape") {
-  //       setOpen(false);
-  //     }
-  //   };
-  //   const hideOnClickOutside = (e) => {
-  //     // console.log(refOne.current);
-  //     // console.log((e.target += 7));
-  //   };
-  //   const [values, setValues] = useState([
-  //     new DateObject().subtract(4, "days"),
-
-  //     // new DateObject().add(11, "days")
-  //   ]);
 
   let footer = <p>Please pick the first day.</p>;
   if (range?.from) {
@@ -92,91 +96,55 @@ function DateTable() {
         touchUi={true}
         style={{ height: 500, margin: "50px" }}
       />
-        <DayPicker
-          mode="range"
-          min={6}
-          max={7}
-          selected={range}
-          onSelect={setRange}
-          footer={footer}
-        />
+
       <Flex>
-        <Button colorScheme="teal" variant="outline" onClick={AddEvent}>
-          Add Event
-        </Button>
-        <Select placeholder="Select" name="task" width={"15%"}>
-          <option value="1">אבט"ש</option>
-          <option value="2">שמירה</option>
-          <option value="3">מחסן</option>
-          <option value="4">אריזת מזון</option>
-          <option value="5">הנפצה</option>
-          <option value="6">אחר</option>
-        </Select>
-
+        <Grid justifyContent="space-around">
+          <Menu>
+            {/*  שאני לוחץ על תורנות הוא השמור לי את התורנות בתוך המודל*/}
+            <MenuButton as={Button}>Actions</MenuButton>
+            <MenuList>
+              {Role.map((item, index) => (
+                <MenuItem key={index} colorScheme="blue" onClick={onOpen}>
+                  {item}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        </Grid>
+        {/* <Button onClick={onOpen}>Open Modal</Button> */}
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <Center>
+              <DayPicker
+                mode="range"
+                min={6}
+                max={7}
+                selected={range}
+                onSelect={setRange}
+                footer={footer}
+              />
+            </Center>
+            <ModalCloseButton />
+            <ModalBody pb={6}></ModalBody>
+            <Input placeholder="comment" />
+            <ModalFooter>
+              {/* 
+<Box>{console.log()}</Box> */}
+//צריך לעשות שלוחצים על שמירה הוא ישמור לי את הטורנות ואת הימים שלו
+              <Button
+                colorScheme="blue"
+                mr={3}
+                variant="outline"
+                onClick={AddEvent}
+              >
+                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
         {console.log(range)}
-
-        {/* 
-        <DatePicker
-       
-       placeholderText="date"
-       range
-      //  value={}
-        onChange={(selectedDates)=>{
-          console.log({selectedDates});
-          if(!selectedDates[1]) return;
-          selectedDates[1].day = selectedDates[0].day+7
-        }}  
-
-        /> */}
-        {/* <DatePicker
-  value={values}
-  onChange={setValues}
-  range
-/> */}
-
-        {/* 
-        <DatePicker
-           placeholderText="Start Date"
-          selectsRange={true}
-          // // Date={new Date().getDate() + 7}
-           selected={newEvent.start}
-          // style={{ width: "200px", height: "50px", border: "3px" }}
-
-           onChange={onChange}
-          // value={value}
-          range
-          weekPicker
-          // selected={startDate} onChange={(date ) => setStartDate(date)}
-        /> */}
-
-        {/* {console.log(startDate)} */}
-        {/* 
-        <Input
-          width="31%"
-          //לסדר את הלוח שנה שיקבל רק 7 ימים ולא מלא
-          value={`  ${format(range[0].startDate, "MM/dd/yyyy")} 
-          to
-           ${format(range[0].endDate, `MM/dd/yyyy` )} `}
-          readOnly
-          className="inputBox"
-          onClick={() => setOpen((open) => !open)}
-        /> */}
-
-        {/* {console.log(range[0].startDate)}
-        {console.log(range[0].endDate)} 
-
-        {open && (
-          <DateRange
-            onChange={(item) => setRange([item.selection])}
-            editableDateInputs={true}
-            moveRangeOnFirstSelection={false}
-            ranges={range}
-            months={1}
-            direction="horizontal"
-            className="calenderElement"
-          
-          />
-        )}  */}
       </Flex>
     </Box>
   );
