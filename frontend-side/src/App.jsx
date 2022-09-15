4ca2c3c65542277940abff318fe2b535ff9c6b85
GandoPlay <edo22061@gmail.com>
2022-09-15 18:32:24 +0300

    cda

--- frontend-side/src/App.jsx
+++ frontend-side/src/App.jsx
@@ -4,21 +4,22 @@ import Login from "./Login/Login";
 import { BrowserRouter } from "react-router-dom";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import FetchRatings, { Rating } from "./Rating/Rating";
+import FetchRatings, { Rating } from "./compoments/Rating";
 import ProtectedRoutes from "./protectedRoutes";
-import DateTable from "./DateTable/DateTable";
-
+import DateTable from "./compoments/DateTable";
+import WithAction from "./navBar/NavBar";
 import history from "./history";
-import NavBar from "./navBar/NavBar";
-import NoneName from "./FoldersProject/noneName";
 
 
 function App() {
   return (
     <BrowserRouter>
+    
       <ChakraProvider>
+      <WithAction/>
+
         <Routes  history={history}>
-        {/* <NavBar/> */}
+
           <Route path="/" element={<Login />} />
           <Route element={<ProtectedRoutes />}>
             <Route path="/rating" element={<FetchRatings />} />
