82c66b52027701f8e8954811573e44c2acda5d03
GandoPlay <edo22061@gmail.com>
2022-09-29 16:35:21 +0300

    sdas

--- frontend-side/src/compoments/NavBar.jsx
+++ frontend-side/src/compoments/NavBar.jsx
@@ -6,85 +6,112 @@ import {
   Link,
   Text,
   useColorModeValue,
-  Alert,
-} from '@chakra-ui/react';
-import {Routes} from '../Constant'
-import { useState } from 'react';
-import useStore from '../appStore';
-import { useDisclosure } from "@chakra-ui/react";
-import { Button } from 'react-day-picker';
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogCloseButton,
+  AlertDialogBody,
+  AlertDialogFooter,
+  Button,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { Routes } from "../Constant";
+import { useRef, useState } from "react";
+import useStore from "../appStore";
 
-
-function removeTokens(){
-  localStorage.removeItem('refreshToken')
-  localStorage.removeItem('accessToken')
+function removeTokens() {
+  localStorage.removeItem("refreshToken");
+  localStorage.removeItem("accessToken");
 }
-const NavLink = ({ title,url }) => (
+const NavLink = ({ title, url }) => (
   <Link
     px={2}
-    onClick = {url==='/'?removeTokens: ()=>{}}
+    onClick={url === "/" ? removeTokens : () => {}}
     py={1}
-    rounded={'md'}
+    rounded={"md"}
     _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
     }}
-    
-    href={url}>
+    href={url}
+  >
     {title}
   </Link>
-
 );
 
-
 export default function NavBar() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
 
-  const username = useStore(state=> state.username)
+  const username = useStore((state) => state.username);
 
-  const [color, setColor] = useState(useColorModeValue('gray.100', 'gray.900'))
+  const [color, setColor] = useState(useColorModeValue("gray.100", "gray.900"));
 
-  if(window.location.pathname!=='/'){
-  return (
-    <>
-    
-      <Box bg={color} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-        <Avatar
-                  size={'md'}
-                  src= 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Hatal.png/180px-Hatal.png'
-                  />
-          
-          <HStack spacing={8} alignItems={'center'}>
-            
-            <HStack
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-              {Object.keys(Routes).map((key) => 
+  if (window.location.pathname !== "/") {
+    return (
+      <>
+        <Box bg={color} px={4}>
+          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+            <Avatar
+              size={"md"}
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Hatal.png/180px-Hatal.png"
+            />
 
-
- 
-
- <NavLink key={key} title = {Routes[key]} url = {key}></NavLink>
-              )
-} 
+            <HStack spacing={8} alignItems={"center"}>
+              <HStack
+                as={"nav"}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+              >
+                {Object.keys(Routes).map((key) =>
+                  key == "/" ? (
+                    <>
+                      <Button onClick={onOpen} fontWeight={'normal'} key={key}>התנתק</Button>
+                      <AlertDialog
+                        motionPreset="slideInBottom"
+                        leastDestructiveRef={cancelRef}
+                        onClose={onClose}
+                        isOpen={isOpen}
+                        isCentered
+                        
+                      >
+                        <AlertDialogOverlay />
+                        <AlertDialogContent>
+                          <AlertDialogHeader  display="flex"  justifyContent="flex-start"  >
+                            התנתק
+                          </AlertDialogHeader>
+                          <AlertDialogCloseButton />
+                          <AlertDialogBody dir="rtl">
+                            האם הנך בטוח שהנך רוצה להתנתק
+                          </AlertDialogBody>
+                          <AlertDialogFooter  justifyContent="space-between">
+                          <Button onClick={onClose}  bg="red.500" key={key} >
+                          <NavLink key={key} title={Routes[key]} url={key} >yes</NavLink>
+                            </Button>
+                            <Button ref={cancelRef} onClick={onClose} bg="green.500">
+                              השאר
+                            </Button>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </>
+                  ) : (
+                    <NavLink key={key} title={Routes[key]} url={key}></NavLink>
+                  )
+                )}
+              </HStack>
             </HStack>
+            <Flex alignItems={"center"}>
+              <Text color="black" mr="30%" w="200px" fontSize="130%"  textAlign="center" >
+                {username} Hello{" "}
+              </Text>
 
-          </HStack>
-          <Flex alignItems={'center'}> 
-          <Text color={'blueviolet'}>{username} שלום </Text>   
-
-                <Avatar
-                  size={'md'}
-                  src={require("./zroa.png")}
-                  />
-                  
+              <Avatar size={"md"} src={require("./zroa.png")} />
+            </Flex>
           </Flex>
-        </Flex>
-      </Box>
-
-    </>
-  );
-}
+        </Box>
+      </>
+    );
+  }
 }
