bdfb6bf65709dbf84061e958acfdc12bc5dd0806
GandoPlay <edo22061@gmail.com>
2022-11-15 10:42:06 +0200

    ffff

--- backend/src/auth/decorator/get-user.decorator.ts
+++ backend/src/auth/decorator/get-user.decorator.ts
@@ -11,12 +11,10 @@ export const GetUser = createParamDecorator(
     const request: Express.Request = ctx
       .switchToHttp()
       .getRequest();
-    console.log(data);
     
     if (data) {
       return request.user[data];
     }
-    console.log(ctx);
 
     return request.user;
   },
